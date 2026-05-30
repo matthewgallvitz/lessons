d618ed27a61134e6a1710c94bc2574f97932b88a
Matthew <mgallvitz@gmail.com>
2026-05-29 18:30:28 -0700

    write get coordinates

--- TTT.py
+++ TTT.py
@@ -16,5 +16,23 @@ for i in range (9):
     turn_1 = not turn_1
 #print(board[2][0])
 
-answer = input("Where would you like to go?")
-print(answer)
+def get_user_coordinates():
+    answer = input("Where would you like to go?")   
+    print(answer)
+    if int(answer) > 9:
+        print("Sorry, but you cannot ask for a number over 9")
+    elif int(answer) < 1:
+        print("Sorry, but you cannot ask for a number under 1")
+    map = {
+        1: [0,0],
+        2: [0,1],
+        3: [0,2],
+        4: [1,0],
+        5: [1,1],
+        6: [1,2],
+        7: [2,0],
+        8: [2,1],
+        9: [2,2],
+    }
+    return map[int(answer)]
+get_user_coordinates() 
